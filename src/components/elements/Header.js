d06b9f0c06a176a1aad1d556d36985c9cb775edc
Anna Laura <alauraivani@gmail.com>
2021-01-30 20:41:20 -0300

    feat: add Header component

--- src/components/elements/Header.js
+++ src/components/elements/Header.js
@@ -1,10 +1,18 @@
 import React from 'react';
 
+import RMDBLogo from '../../assets/images/reactMovie_logo.png';
+import TMDBLobo from '../../assets/images/tmdb_logo.svg';
+import {StyledHeader, StyledRMDBLogo, StyledTMDBLogo} from '../../assets/styles/styledHeader';
+
+
 const Header = () => {
   return(
-    <div>
-      Header
-    </div>
+    <StyledHeader>
+      <div className="header-content">
+        <StyledRMDBLogo src={RMDBLogo} alt="rmdb-logo"/>
+        <StyledTMDBLogo src={TMDBLobo} alt="tmdb-logo"/>
+      </div>
+    </StyledHeader>
   )
 }
 
