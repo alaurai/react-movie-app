4c71829f49598cb95bbd8c076a7c56de37f9ae81
Anna Laura <alauraivani@gmail.com>
2021-01-31 13:38:22 -0300

    feat: add Spinner component

---
+++ src/components/elements/Spinner.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { StyledSpinner } from '../../assets/styles/StyledSpinner';
+
+const Spinner = () => {
+  return(
+    <StyledSpinner/>
+  )
+}
+
+export default Spinner;
