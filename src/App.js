ec86c4e031f8d6b4208193345948247d0b7cb4a9
Anna Laura <alauraivani@gmail.com>
2021-01-31 13:20:59 -0300

    feat: add global style

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import { GlobalStyle } from './assets/styles/GlobalStyle';
+
 import Header from './components/elements/Header';
 import Home from './components/Home';
 
@@ -6,6 +9,7 @@ function App() {
     <div className="App">
       <Header/>
       <Home/>
+      <GlobalStyle/>
     </div>
   );
 }
